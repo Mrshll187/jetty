dad86f9c9079b244453b924129e762eebf50fe2e
xxx <xxx@xxxs-MBP.fios-router.home>
2018-08-18 09:53:14 -0400

    Removed old class

--- src/main/java/xxx/App.java
+++ src/main/java/xxx/App.java
@@ -1,11 +1,36 @@
 package xxx;
 
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
 import xxx.xxx.management.LifeCycleManager;
 
 public class App {
-    
-	public static void main(String[] args) throws Exception {
-		
-		LifeCycleManager.start();	
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
+
+        resource_handler.setResourceBase(".");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+        server.setHandler(handlers);
+
+        server.start();
+        server.join();
+        
+        LifeCycleManager.start();
     }
 }
